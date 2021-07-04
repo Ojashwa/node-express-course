5f532ac2c0d00cad2b9f960780b8c40a5e83d9ee
Ojashwa Saini <sojs220@gmail.com>
2021-07-04 12:16:55 +0530

    node day 1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req, res)=>{
+  console.log('user hit the resource');
+  res.send('home page');
+})
+app.listen(5000,()=>{
+  console.log('server is listening on port 5000');
+})
